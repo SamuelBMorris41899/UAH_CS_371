7da98d2b8104dd8b0514a1269cf7e49b2616c659
SamuelBMorris41899 <43507180+SamuelBMorris41899@users.noreply.github.com>
2021-10-26 13:03:28 -0500

    starting setup

--- TheGame/butter.lua
+++ TheGame/butter.lua
@@ -1,14 +1,31 @@
 butter = {}
 
+
+butter.state = 0
+butterSpriteSheet = {}
+
+
+local animationStateManager ={
+    {name="Nominal", start=1, count=4, time=700, loopCount=0},
+}
+
+--create Ryu and set the idle animation to start
+
+
+
+
+
+
 function init() 
-    newButter = {}
-    newButter = deepAppend(butter,newButter)
-
-
-
+    newButter = display.newSprite(butterSpriteSheet, animationStateManager)
+    newButter = deepAppend(butter,newButter) --adds all functions,tables and variables from butter to the new butter
 end
 
 function butter.tick(self) 
 
+end
 
+function butter.setState(self,state)
+    self:setSequence(state)
+    self:play()
 end
