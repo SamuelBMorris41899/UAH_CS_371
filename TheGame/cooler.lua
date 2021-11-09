5a940bc561a1cfc0bb43bdadedefc723b881554a
SamuelBMorris41899 <43507180+SamuelBMorris41899@users.noreply.github.com>
2021-11-09 14:13:07 -0600

    Cooler added
    there is now a cooler object in the enviroment

---
+++ TheGame/cooler.lua
@@ -0,0 +1,54 @@
+require("utility")
+builder = {}
+
+builder.On = true
+
+
+local frameInfo = { 
+    frames = {
+        {x = 1, y = 2, width = 87, height = 131}, --1
+        {x = 91, y = 2, width = 87, height = 131},--2
+        {x = 187, y = 2, width = 87, height = 131},--3
+    }
+}
+local butterSheet = graphics.newImageSheet( "Cooler.png", frameInfo )
+
+
+--This is the controller for the sprite animations
+local butterStateManager ={
+    {name="off", start=1, count=1, time=1, loopCount=1},
+    {name="on", start=2, count=2, time=700, loopCount=0},
+}
+
+
+
+function builder.init(self,x,y) 
+    
+
+    new = display.newSprite(butterSheet, butterStateManager)
+    
+    new = deepAppend(builder,new)
+    new:changeAnim()
+    new.x = x
+    new.y = y
+    return new
+end
+
+function builder.changeAnim(self)
+
+    if (self.On) then 
+        print("ON!")
+        self:setSequence("on")
+    else 
+        print("Off")
+        self:setSequence("off")
+    end
+    self:play()
+end
+
+function builder.turnOnOff(self) 
+    self.On = not self.On
+end
+
+
+return builder
--- TheGame/rocketEnviroment.lua
+++ TheGame/rocketEnviroment.lua
@@ -3,8 +3,16 @@ globalTemp = 105
 require("butter")
 butter = butterBuilder:init()
 
-function tick() 
-    butter:tick()    
+
+--TEST AREA
+coolerBuilder = require("cooler")
+
+coolerBuilder:init(200,200)
+
+coolerBuilder:init(100,200)
+
+function tick()
+    butter:tick()
 end
 
 gameLoopTimer = timer.performWithDelay( 1000, tick, 0)
