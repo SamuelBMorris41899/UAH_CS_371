8f7e51d13e4ba9ba5e1152a003ea1aaf5a685da4
SamuelBMorris41899 <43507180+SamuelBMorris41899@users.noreply.github.com>
2021-11-11 17:09:01 -0600

    Added Events and small bug fix
    -Fixed pause to level seclect bug
    -Added Events

--- TheGame/events/events.lua
+++ TheGame/events/events.lua
@@ -1,6 +1,13 @@
+local math =require("math")
 events = {}
 
+eclipse = require("Eclipse")
+table.insert( events, eclipse )
 
+solarFlare =require("SolarStorm")
+table.insert( events, solarFlare )
 
-
+function getRandomEvent()
+    return events[math.random(#myTable)]
+end
 return events;
